ef80bffd496009cec5b6d5f4981e5d2dc1ce452f
Jessica Meixner <Jessica.Meixner@noaa.gov>
2026-05-18 16:36:18 +0000

    variables are defined in modules so we do not need to set them

--- sorc/make_NWPS.sh
+++ sorc/make_NWPS.sh
@@ -94,29 +94,6 @@ module list
 
 mkdir -p ${NWPSdir}/exec
 
-#FOR NETCDF
-echo "================== FOR NETCDF : make_netcdf-4.2.sh =================="
-#cd ${NWPSdir}/lib
-#./make_netcdf-4.2.sh | tee ./netcdf-4.2_build.log
-
-#export NETCDF=${NWPSdir}/lib/netcdf/${netcdf_ver}
-#export NETCDF_ROOT=${NWPSdir}/lib/netcdf/${netcdf_ver}
-#export HDF5_ROOT=${NWPSdir}/lib/hdf5/${hdf5_ver}
-#export NETCDF_INC=${NWPSdir}/lib/netcdf/${netcdf_ver}/include
-#export HDF5_INC=${NWPSdir}/lib/hdf5/${hdf5_ver}/include
-#export NETCDF_INCLUDES=${NWPSdir}/lib/netcdf/${netcdf_ver}/include
-#export NETCDF_LIBRARIES=${NWPSdir}/lib/netcdf/${netcdf_ver}/lib
-#export HDF5_LIBRARIES=${NWPSdir}/lib/hdf5/${hdf5_ver}/lib
-
-export NETCDF=/apps/prod/hpc-stack/i-19.1.3.304__m-8.1.19__h-1.14.0__n-4.9.2__p-2.5.10__e-8.8.0_pnetcdf/intel-19.1.3.304/cray-mpich-8.1.19/netcdf/4.9.2
-export NETCDF_ROOT=/apps/prod/hpc-stack/i-19.1.3.304__m-8.1.19__h-1.14.0__n-4.9.2__p-2.5.10__e-8.8.0_pnetcdf/intel-19.1.3.304/cray-mpich-8.1.19/netcdf/4.9.2
-export HDF5_ROOT=/apps/prod/hpc-stack/i-19.1.3.304__m-8.1.19__h-1.14.0__n-4.9.2__p-2.5.10__e-8.8.0_pnetcdf/intel-19.1.3.304/cray-mpich-8.1.19/hdf5/1.14.0/
-export NETCDF_INC=${NETCDF_ROOT}/include
-export HDF5_INC=${HDF5_ROOT}/include
-export NETCDF_INCLUDES=${NETCDF_ROOT}/include
-export NETCDF_LIBRARIES=${NETCDF_ROOT}/lib
-export HDF5_LIBRARIES=${HDF5_ROOT}/lib
-
 #FOR SWAN (REGULAR GRID)
 echo "================== FOR SWAN (REGULAR GRID) : make_swan.sh  =================="
 cd ${NWPSdir}/sorc
