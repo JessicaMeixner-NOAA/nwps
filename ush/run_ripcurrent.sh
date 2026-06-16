b62b2568a87798ba15b658f406a86bd878372596
Jessica Meixner <Jessica.Meixner@noaa.gov>
2026-06-16 17:54:03 +0000

    only copy riphist files if they exist

--- ush/run_ripcurrent.sh
+++ ush/run_ripcurrent.sh
@@ -251,10 +251,15 @@ cp ${RIPDATA}/${contour}m_RipForecastShoreline_${SITEID}.txt ${RIPDATA}/fort.21
 # ======================================================================
 # Copy the data from the previous 72 hrs
 # ======================================================================
-cp -f $GESINm3/riphist/${SITEID}/${contour}*${CGnumber}*${SITEID} ${RIPDATA}
-cp -f $GESINm4/riphist/${SITEID}/${contour}*${CGnumber}*${SITEID} ${RIPDATA}
-cp -f $GESINm5/riphist/${SITEID}/${contour}*${CGnumber}*${SITEID} ${RIPDATA}
-
+if [[ -f ${GESINm3}/riphist/${SITEID}/${contour}*${CGnumber}*${SITEID} ${RIPDATA} ]]; then  
+  cp -f ${GESINm3}/riphist/${SITEID}/${contour}*${CGnumber}*${SITEID} ${RIPDATA}
+fi 
+if [[ -f ${GESINm4}/riphist/${SITEID}/${contour}*${CGnumber}*${SITEID} ]]; then 
+  cp -f ${GESINm4}/riphist/${SITEID}/${contour}*${CGnumber}*${SITEID} ${RIPDATA}
+fi 
+if [[ -f ${GESINm5}/riphist/${SITEID}/${contour}*${CGnumber}*${SITEID} ${RIPDATA} ]]; then 
+  cp -f ${GESINm5}/riphist/${SITEID}/${contour}*${CGnumber}*${SITEID} ${RIPDATA}
+fi 
 # ======================================================================
 # Create output file and add header
 # ======================================================================
